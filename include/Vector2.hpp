e1cbcfc43f47e49a63ed7974649eee11a8a7ebeb
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-28 21:30:20 +0200

    Add and implement Vector2

---
+++ include/Vector2.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <ostream>
+
+class Vector2
+{
+private:
+    double _x, _y;
+
+public:
+    Vector2();
+    Vector2(int x, int y);
+    Vector2(double x, double y);
+
+    bool orthogonal(const Vector2 &vec) const;
+    bool parallel(const Vector2 &vec) const;
+    Vector2 normalize() const;
+
+    double x() const;
+    double y() const;
+    friend Vector2 operator+(const Vector2 &lhs, const Vector2 &rhs);
+    friend Vector2 operator-(const Vector2 &lhs, const Vector2 &rhs);
+    friend double operator*(const Vector2 &lhs, const Vector2 &rhs);
+    friend Vector2 operator*(const Vector2 &lhs, int length);
+    friend Vector2 operator*(const Vector2 &lhs, float length);
+    friend Vector2 operator*(const Vector2 &lhs, double length);
+    friend Vector2 operator*(int length, const Vector2 &lhs);
+    friend Vector2 operator*(float length, const Vector2 &lhs);
+    friend Vector2 operator*(double length, const Vector2 &lhs);
+    friend Vector2 operator/(const Vector2 &lhs, const Vector2 &rhs);
+    friend bool operator==(const Vector2 &lhs, const Vector2 &rhs);
+    friend std::ostream &operator<<(std::ostream &out, const Vector2 point);
+};
---
+++ src/Vector2.cpp
@@ -0,0 +1,94 @@
+#include "Vector2.hpp"
+#include "math.h"
+
+Vector2::Vector2() : _x{0}, _y{0} {};
+Vector2::Vector2(int x, int y) : _x{(double)x}, _y{(double)y} {};
+Vector2::Vector2(double x, double y) : _x{x}, _y{y} {};
+
+double Vector2::x() const
+{
+    return _x;
+}
+
+double Vector2::y() const
+{
+    return _y;
+}
+
+Vector2 operator+(const Vector2 &lhs, const Vector2 &rhs)
+{
+    return Vector2(lhs.x() + rhs.x(), lhs.y() + rhs.y());
+}
+
+Vector2 operator-(const Vector2 &lhs, const Vector2 &rhs)
+{
+    return Vector2(lhs.x() - rhs.x(), lhs.y() - rhs.y());
+}
+
+double operator*(const Vector2 &lhs, const Vector2 &rhs)
+{
+    return lhs.x() * rhs.x() + lhs.y() * rhs.y();
+}
+
+Vector2 operator*(const Vector2 &lhs, int length)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator*(const Vector2 &lhs, float length)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator*(const Vector2 &lhs, double length)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator*(int length, const Vector2 &lhs)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator*(float length, const Vector2 &lhs)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator*(double length, const Vector2 &lhs)
+{
+    return Vector2(lhs.x() * length, lhs.y() * length);
+}
+
+Vector2 operator/(const Vector2 &lhs, const Vector2 &rhs)
+{
+    return Vector2(lhs.x() / rhs.x(), lhs.y() / rhs.y());
+}
+
+bool operator==(const Vector2 &lhs, const Vector2 &rhs)
+{
+    return (lhs.x() == rhs.x() && lhs.y() == rhs.y());
+}
+
+bool Vector2::orthogonal(const Vector2 &vec) const
+{
+    return ((*this) * vec) == 0;
+}
+
+Vector2 Vector2::normalize() const
+{
+    double length = std::sqrt(_x * _x + _y * _y);
+    Vector2 result{*this};
+    return result * (1 / length);
+}
+
+bool Vector2::parallel(const Vector2 &vec) const
+{
+    return (this->normalize() == vec.normalize());
+}
+
+std::ostream &operator<<(std::ostream &stream, Vector2 point)
+{
+    stream << "(" << point.x() << ", " << point.y() << ")";
+    return stream;
+}
