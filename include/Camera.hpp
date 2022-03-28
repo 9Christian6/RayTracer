f80c0bfa60fd599a1f2e6f105a8b23d6df316dcc
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-28 21:54:15 +0200

    Add and implement Camera

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include "Vector.hpp"
+#include "Vector2.hpp"
 #include "Ray.hpp"
 
 class Camera
 {
 private:
-    Vector _position, _forward, _up;
+    Vector _position, _forward, _up, _right;
+    double h, w;
 
 public:
-    Camera();
-    Ray makeRay(Vector point) const;
+    Camera(Vector origin, Vector target, Vector upguide, double fov, double aspectRatio);
+    Ray makeRay(Vector2 point) const;
 };
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,3 +1,17 @@
 #include "Camera.hpp"
+#include "math.h"
 
-Camera::Camera(){};
+Camera::Camera(Vector origin, Vector target, Vector upguide, double fov, double aspectRatio) : _position{origin}
+{
+    _forward = (target - origin).normalize();
+    _right = _forward.cross(upguide).normalize();
+    _up = _right.cross(_forward);
+    h = std::tan(fov);
+    w = h * aspectRatio;
+};
+
+Ray Camera::makeRay(Vector2 point) const
+{
+    Vector direction = _forward + point.x() * w * _right + point.y() * h * _up;
+    return Ray{_position, direction.normalize()};
+}
