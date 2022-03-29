666d89b2c4072cda1c96bf8e4bd680772b05fdb0
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-29 19:46:35 +0200

    Do stuff in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
-#include <gmpxx.h>
-#include <Eigen/Core>
 #include "Ray.hpp"
 #include "Vector.hpp"
-#include "Plane.hpp"
 #include "Exception.hpp"
 #include "Sphere.hpp"
+#include "Camera.hpp"
+#include "Image.hpp"
 #include <cmath>
+// std::cout.setf(std::ios::boolalpha);
 
 int main(int, char **)
 {
-    std::cout.setf(std::ios::boolalpha);
     Vector origin{0, 0, 0}, xVec{1, 0, 0}, yVec{0, 1, 0}, zVec{0, 0, 1};
-    Ray xRay{origin, xVec};
-    Plane xzPlane{origin, yVec};
-    Sphere sphere{origin, 1};
-    Ray testRay{origin, xVec + yVec};
-    std::cout << testRay.hit(sphere).value() << "\n";
+    Camera camera{yVec, zVec + yVec, 2 * yVec, 90, 1};
+    Image image{100, 100};
+    Sphere sphere{10 * zVec, 5};
+    auto xzPlane = Plane{origin, yVec};
+    for (int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            auto ray = camera.makeRay(image, Vector2{x, y});
+            auto hit = ray.hit(sphere);
+            if (hit)
+            {
+                image.setPixel(x, y, true);
+                continue;
+            }
+            std::cout << ray << "\n";
+        }
+    }
+    image.print();
 }
