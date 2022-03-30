912b5d7fd86f673cdffaec5367c99c325ce71137
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-30 17:50:44 +0200

    Test stuff

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "Sphere.hpp"
 #include "Camera.hpp"
 #include "Image.hpp"
+#include "Intersection.hpp"
 #include <cmath>
 // std::cout.setf(std::ios::boolalpha);
 
@@ -12,7 +13,7 @@ int main(int, char **)
 {
     Vector origin{0, 0, 0}, xVec{1, 0, 0}, yVec{0, 1, 0}, zVec{0, 0, 1};
     Camera camera{yVec, zVec + yVec, 2 * yVec, 90, 1};
-    Image image{100, 100};
+    Image image{10, 10};
     Sphere sphere{10 * zVec, 5};
     auto xzPlane = Plane{origin, yVec};
     for (int x = 0; x < image.width(); x++)
@@ -21,12 +22,9 @@ int main(int, char **)
         {
             auto ray = camera.makeRay(image, Vector2{x, y});
             auto hit = ray.hit(sphere);
-            if (hit)
-            {
-                image.setPixel(x, y, true);
-                continue;
-            }
-            std::cout << ray << "\n";
+            Intersection hitPoint{ray};
+            sphere.intersect(hitPoint);
+            std::cout << hitPoint.t() << "\n";
         }
     }
     image.print();
