579e0444a40e650709dbf4c7a6e1ec86fb93046b
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-30 18:20:45 +0200

    Add functions

--- src/Shapeset.cpp
+++ src/Shapeset.cpp
@@ -4,3 +4,24 @@ void ShapeSet::addShape(Shape &shape)
 {
     _shapes.push_back(&shape);
 }
+
+bool ShapeSet::doesIntersect(const Ray &ray)
+{
+    for (auto shape : _shapes)
+    {
+        if (shape->doesIntersect(ray))
+            return true;
+    }
+    return false;
+}
+
+bool ShapeSet::intersect(Intersection &intersection)
+{
+    bool doesIntersect = false;
+    for (auto shape : _shapes)
+    {
+        if (shape->intersect(intersection))
+            doesIntersect = true;
+    }
+    return doesIntersect;
+}
