6bde943d9c6e3fcb3eb512c1473d5a0ae2af95b0
9Christian6 <chrissi.schulz@googlemail.com>
2022-03-30 15:44:58 +0200

    Implement Intersection

--- include/Intersection.hpp
+++ include/Intersection.hpp
@@ -1,11 +1,20 @@
 #pragma once
+#include "Vector.hpp"
 #include "Ray.hpp"
+#include <memory>
+
+class Shape;
 
 class Intersection
 {
 private:
-    Ray ray;
-    double t;
+    const Ray &_ray;
+    double _t;
+    const Shape *_pShape;
 
 public:
+    Intersection(const Ray &ray, const Shape &shape);
+    Intersection(const Ray &ray);
+    bool intersected() const;
+    Vector position();
 };
--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -1,1 +1,21 @@
 #include "Intersection.hpp"
+#include "Ray.hpp"
+
+Intersection::Intersection(const Ray &ray, const Shape &shape) : _ray{ray}
+{
+    _pShape = &shape;
+}
+
+Intersection::Intersection(const Ray &ray) : _ray{ray}, _t{Ray::RAY_T_MAX}, _pShape{NULL}
+{
+}
+
+bool Intersection::intersected() const
+{
+    return (_pShape != NULL);
+}
+
+Vector Intersection::position()
+{
+    return _ray.getPoint(_t);
+}
