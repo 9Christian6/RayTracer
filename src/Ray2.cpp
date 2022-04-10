88de0ccf31a17639d2465de3fbdb4d077c1ff0a1
9Christian6 <chrissi.schulz@googlemail.com>
2022-04-10 19:21:37 +0200

    add namespace raytracer

--- src/Ray2.cpp
+++ src/Ray2.cpp
@@ -4,45 +4,49 @@
 #include "fEquals.hpp"
 #include "SafeDivision.hpp"
 #include <ostream>
-Ray2::Ray2(Vector2 origin, Vector2 direction) : _origin{origin}, _direction{direction}
-{
-    if (direction == Vector2{0, 0})
-        throw Exception{"Direction can't be zero"};
-    _direction = _direction.normalize();
-};
 
-Vector2 Ray2::origin() const
+namespace raytracer
 {
-    return Vector2{_origin};
-}
+    Ray2::Ray2(Vector2 origin, Vector2 direction) : _origin{origin}, _direction{direction}
+    {
+        if (direction == Vector2{0, 0})
+            throw Exception{"Direction can't be zero"};
+        _direction = _direction.normalize();
+    };
 
-Vector2 Ray2::direction() const
-{
-    return Vector2{_direction};
-}
+    Vector2 Ray2::origin() const
+    {
+        return Vector2{_origin};
+    }
 
-Vector2 Ray2::getPoint(double t) const
-{
-    return _origin + (t * _direction);
-}
+    Vector2 Ray2::direction() const
+    {
+        return Vector2{_direction};
+    }
 
-std::optional<double> Ray2::getT(const Vector2 &point) const
-{
-    if (raytracer::equals(_direction.x(), 0) && raytracer::equals(_origin.x(), point.x()))
-        return raytracer::scalarQuotient(_direction.y(), point.y() - _origin.y());
-    if (raytracer::equals(_direction.y(), 0) && raytracer::equals(point.y(), _origin.y()))
-        return raytracer::scalarQuotient(_direction.x(), point.x() - _origin.x());
-    std::optional<double> t;
-    auto t1 = raytracer::scalarQuotient(_direction.x(), point.x() - _origin.x());
-    auto t2 = raytracer::scalarQuotient(_direction.y(), point.y() - _origin.y());
-    if (t1 && t2 && (raytracer::equals(*t1, *t2)))
-        t = t1;
-    return t;
-}
+    Vector2 Ray2::getPoint(double t) const
+    {
+        return _origin + (t * _direction);
+    }
 
-std::ostream &operator<<(std::ostream &out, const Ray2 &ray)
-{
-    out << "Ray2\n o: " << ray.origin() << ", d: " << ray.direction();
-    out.flush();
-    return out;
+    std::optional<double> Ray2::getT(const Vector2 &point) const
+    {
+        if (equals(_direction.x(), 0) && equals(_origin.x(), point.x()))
+            return scalarQuotient(_direction.y(), point.y() - _origin.y());
+        if (equals(_direction.y(), 0) && equals(point.y(), _origin.y()))
+            return scalarQuotient(_direction.x(), point.x() - _origin.x());
+        std::optional<double> t;
+        auto t1 = scalarQuotient(_direction.x(), point.x() - _origin.x());
+        auto t2 = scalarQuotient(_direction.y(), point.y() - _origin.y());
+        if (t1 && t2 && (equals(*t1, *t2)))
+            t = t1;
+        return t;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Ray2 &ray)
+    {
+        out << "Ray2\n o: " << ray.origin() << ", d: " << ray.direction();
+        out.flush();
+        return out;
+    }
 }
