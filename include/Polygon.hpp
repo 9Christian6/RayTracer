7b8b20721ad549fee067d2c9cd31ecc9b46d5210
9Christian6 <chrissi.schulz@googlemail.com>
2022-04-11 16:47:41 +0200

    Add and implement polygon

--- include/Polygon.hpp
+++ include/Polygon.hpp
@@ -1,13 +1,25 @@
 #pragma once
 #include "Vector.hpp"
+#include "Shape.hpp"
+#include "Ray.hpp"
+#include "Intersection.hpp"
+#include "Line2.hpp"
 #include <vector>
+#include <optional>
+#include <ostream>
 
-class Polygon
+namespace raytracer
 {
-private:
-    std::vector<Vector> _points;
+    class Polygon : public Shape
+    {
+    private:
+        std::vector<Vector> _points;
 
-public:
-    Polygon() = delete;
-    Polygon(std::vector<Vector> points);
-};
+    public:
+        Polygon() = delete;
+        Polygon(std::vector<Vector> points);
+        std::vector<Vector> points() const;
+        std::optional<Intersection> intersect(const Ray &ray) override;
+        friend std::ostream &operator<<(std::ostream &out, const Polygon &poly);
+    };
+}
--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -1,6 +1,132 @@
+#include "Ray.hpp"
+#include "Intersection.hpp"
 #include "Polygon.hpp"
+#include "Exception.hpp"
+#include "Plane.hpp"
+#include "Vector.hpp"
+#include "Vector2.hpp"
+#include "Line2.hpp"
+#include "Ray2.hpp"
+#include <iostream>
 #include <vector>
 
-Polygon::Polygon(std::vector<Vector> points) : _points(points)
+namespace raytracer
 {
+    Polygon::Polygon(std::vector<Vector> points) : _points(points)
+    {
+        auto plane = Plane{_points.at(0), _points.at(1), _points.at(2)};
+        for (auto point : _points)
+        {
+            if (!plane.conatins(point))
+                throw Exception{"Not all points lie in one plane"};
+        }
+    }
+
+    // bool Polygon::doesIntersect(const Ray &ray)
+    // {
+    //     auto plane = Plane{_points.at(0), _points.at(1), _points.at(2)};
+    //     auto hitPoint = ray.hit(plane);
+    //     if (!hitPoint)
+    //         return false;
+    //     auto hit = hitPoint.value();
+    //     double u, v;
+    //     double nX{plane.normal().x()}, nY{plane.normal().y()}, nZ{plane.normal().z()};
+    //     int dimToLoose;
+    //     if (nX >= nY && nX >= nZ)
+    //     {
+    //         dimToLoose = 0;
+    //         u = nY;
+    //         v = nZ;
+    //     }
+    //     if (nY >= nX && nY >= nZ)
+    //     {
+    //         dimToLoose = 1;
+    //         u = nX;
+    //         v = nZ;
+    //     }
+    //     if (nZ >= nX && nZ >= nY)
+    //     {
+    //         dimToLoose = 2;
+    //         u = nX;
+    //         v = nY;
+    //     }
+    //     std::vector<Vector2> projectedPoints;
+    //     for (auto point : _points)
+    //     {
+    //         Vector2 projected{point.project(dimToLoose)};
+    //         projected = projected - Vector2{u, v};
+    //         projectedPoints.push_back(point.project(dimToLoose));
+    //     }
+    //     for (size_t i = 1; i < projectedPoints.size(); i++)
+    //     {
+    //     }
+    //     return false;
+    // }
+
+    std::optional<Intersection> Polygon::intersect(const Ray &ray)
+    {
+        Plane plane{_points.at(0), _points.at(1), _points.at(2)};
+        // auto hitPoint = plane.intersect(ray);
+        // if (!hitPoint)
+        //     return hitPoint;
+        // auto hit = hitPoint.value();
+        if (!plane.intersect(ray))
+            return std::optional<Intersection>{};
+        auto hit = plane.intersect(ray).value();
+        double nX{std::abs(plane.normal().x())}, nY{std::abs(plane.normal().y())}, nZ{std::abs(plane.normal().z())};
+        int dimToLoose;
+        if (nX >= nY && nX >= nZ)
+        {
+            dimToLoose = 0;
+        }
+        if (nY >= nX && nY >= nZ)
+        {
+            dimToLoose = 1;
+        }
+        if (nZ >= nX && nZ >= nY)
+        {
+            dimToLoose = 2;
+        }
+        std::optional<Intersection> hitPoint;
+        auto projectedHit = hit.position().project(dimToLoose);
+        int intersectionCount = 0;
+        Ray2 testRay{projectedHit, Vector2{1, 0}};
+        std::vector<Line2> lines;
+        Vector2 p0 = _points[0].project(dimToLoose);
+        Vector2 lastPoint = _points.at(_points.size() - 1).project(dimToLoose);
+        lines.push_back(Line2{p0, lastPoint});
+        for (size_t i = 1; i < _points.size(); i++)
+        {
+            Vector2 p1{_points[i - 1].project(dimToLoose)}, p2{_points[i].project(dimToLoose)};
+            lines.push_back(Line2{p1, p2});
+        }
+        for (auto line : lines)
+            if (auto hit = line.intersect(testRay))
+            {
+                intersectionCount++;
+            }
+        if (intersectionCount % 2 == 1)
+        {
+            hitPoint.emplace(hit);
+        }
+        return hitPoint;
+    }
+
+    std::vector<Vector> Polygon::points() const
+    {
+        return _points;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Polygon &poly)
+    {
+        std::vector<Vector> points = poly.points();
+        out << "Polygon\n"
+            << points.at(0);
+        for (size_t i = 1; i < poly.points().size(); i++)
+        {
+            out << ", " << points.at(i);
+        }
+        out << "\n";
+        return out;
+    }
 }
