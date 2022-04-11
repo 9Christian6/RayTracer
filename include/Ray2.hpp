66ef9295901562d92cc16d3a7eae57fda73b6b25
9Christian6 <chrissi.schulz@googlemail.com>
2022-04-11 16:02:08 +0200

    Rename getPoint to scale

--- include/Ray2.hpp
+++ include/Ray2.hpp
@@ -15,7 +15,7 @@ namespace raytracer
 
         Vector2 origin() const;
         Vector2 direction() const;
-        Vector2 getPoint(double t) const;
+        Vector2 scale(double t) const;
         std::optional<double> getT(const Vector2 &point) const;
         friend std::ostream &operator<<(std::ostream &out, const Ray2 &ray);
     };
--- src/Ray2.cpp
+++ src/Ray2.cpp
@@ -24,18 +24,18 @@ namespace raytracer
         return Vector2{_direction};
     }
 
-    Vector2 Ray2::getPoint(double t) const
+    Vector2 Ray2::scale(double t) const
     {
         return _origin + (t * _direction);
     }
 
     std::optional<double> Ray2::getT(const Vector2 &point) const
     {
+        std::optional<double> t;
         if (equals(_direction.x(), 0) && equals(_origin.x(), point.x()))
             return scalarQuotient(_direction.y(), point.y() - _origin.y());
         if (equals(_direction.y(), 0) && equals(point.y(), _origin.y()))
             return scalarQuotient(_direction.x(), point.x() - _origin.x());
-        std::optional<double> t;
         auto t1 = scalarQuotient(_direction.x(), point.x() - _origin.x());
         auto t2 = scalarQuotient(_direction.y(), point.y() - _origin.y());
         if (t1 && t2 && (equals(*t1, *t2)))
