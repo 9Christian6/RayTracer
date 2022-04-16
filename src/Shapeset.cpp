efb43909552b44e6ac605cfafce36c838e3b20d0
9Christian6 <chrissi.schulz@googlemail.com>
2022-04-16 15:47:59 +0200

    only return smallest intersection

--- src/Shapeset.cpp
+++ src/Shapeset.cpp
@@ -1,7 +1,15 @@
 #include "Shapeset.hpp"
+#include <bits/stdc++.h>
+#include <vector>
 
 namespace raytracer
 {
+
+    bool compareIntersection(const Intersection &lhs, const Intersection &rhs)
+    {
+        return lhs.t() < rhs.t();
+    }
+
     void ShapeSet::addShape(Shape &shape)
     {
         _shapes.push_back(&shape);
@@ -9,11 +17,29 @@ namespace raytracer
 
     std::optional<Intersection> ShapeSet::intersect(const Ray &ray)
     {
+        std::vector<Intersection> hits;
         for (auto shape : _shapes)
         {
             if (auto hitPoint = shape->intersect(ray))
-                return hitPoint;
+            {
+                hits.push_back(hitPoint.value());
+            }
         }
-        return std::optional<Intersection>{};
+        if (hits.size() == 0)
+            return {};
+        std::size_t temp = 0;
+        for (std::size_t i = 0; i < hits.size(); i++)
+        {
+            if (hits[temp].t() > hits[i].t())
+                temp = i;
+        }
+        return hits[temp];
+        // if (hits.size() > 0)
+        //     return hits.front();
+        // if (hits.size() > 0)
+        // {
+        //     std::sort(hits.begin(), hits.end(), compareIntersection);
+        //     return hits.front();
+        // }
     }
 }
