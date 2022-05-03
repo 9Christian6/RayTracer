b8d7da592c2ca4857ab6ea65e96e4b505669d83e
9Christian6 <chrissi.schulz@googlemail.com>
2022-05-03 09:50:18 +0200

    isVisible takes point, reorder stuff

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -9,48 +9,6 @@ namespace raytracer
     Scene::Scene(std::vector<Light> lights, ShapeSet &shapes, Camera &cam)
         : _lights{lights}, _shapes{shapes}, _camera{cam} {};
 
-    void Scene::render(int width, int height) const
-    {
-        auto img = Image{width, height};
-        for (int x = 0; x <= width; x++)
-        {
-            for (int y = 0; y <= height; y++)
-            {
-                auto ray = _camera.makeRay(width, height, Vector2{x, y});
-                if (auto intersection = _shapes.intersect(ray))
-                {
-                    auto lights = visibleLights(*intersection);
-                    if (auto hitColor = intersection->color(lights))
-                        img.plot(x, y, *hitColor);
-                }
-            }
-        }
-        img._image.close();
-    }
-
-    bool Scene::isVisible(const Intersection &point, const Light &light) const
-    {
-        Vector position = point.position() + Ray::RAY_T_MIN * point.normal();
-        Ray lightRay{light.position(), position - light.position()};
-        double tLength{(light.position() - point.position()).length()};
-        if (auto lightHit = _shapes.intersect(lightRay))
-        {
-            return (tLength < lightHit->t());
-        }
-        return true;
-    }
-
-    std::vector<Light> Scene::visibleLights(const Intersection &point) const
-    {
-        std::vector<Light> lights{};
-        for (auto light : _lights)
-        {
-            if (isVisible(point, light))
-                lights.push_back(light);
-        }
-        return lights;
-    }
-
     void Scene::addShape(Shape &shape)
     {
         _shapes.addShape(shape);
@@ -66,9 +24,44 @@ namespace raytracer
         _camera.setPosition(position, upGuide, forward);
     }
 
-    std::optional<Intersection> Scene::specular(const Intersection &hit) const
+    void Scene::render(int width, int height) const
     {
-        auto reflectedRay = hit.reflectionRay();
-        return _shapes.intersect(reflectedRay);
+        auto img = Image{width, height};
+        for (int x = 0; x <= width; x++)
+        {
+            for (int y = 0; y <= height; y++)
+            {
+                auto ray = _camera.makeRay(width, height, Vector2{x, y});
+                if (auto intersection = _shapes.intersect(ray))
+                {
+                    auto lights = visibleLights(intersection->position());
+                    if (auto hitColor = intersection->color(lights))
+                        img.plot(x, y, *hitColor);
+                }
+            }
+        }
+        img._image.close();
+    }
+
+    bool Scene::isVisible(const Vector &point, const Light &light) const
+    {
+        Ray lightRay{light.position(), point - light.position()};
+        double tLength{(light.position() - point).length()};
+        if (auto lightHit = _shapes.intersect(lightRay))
+        {
+            return (tLength < lightHit->t() + Ray::RAY_T_MIN);
+        }
+        return true;
+    }
+
+    std::vector<Light> Scene::visibleLights(const Vector &point) const
+    {
+        std::vector<Light> lights{};
+        for (auto light : _lights)
+        {
+            if (isVisible(point, light))
+                lights.push_back(light);
+        }
+        return lights;
     }
 }
