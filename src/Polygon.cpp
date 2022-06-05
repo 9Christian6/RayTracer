99f14219758c61530e1c9f579c2dfad021cd9e82
9Christian6 <chrissi.schulz@googlemail.com>
2022-06-05 18:42:25 +0200

    initialize BoundingBox in constructor

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -14,11 +14,25 @@ namespace raytracer
 {
     Polygon::Polygon(std::vector<Vector> points) : _points(points)
     {
+        double xMin{0}, xMax{0}, yMin{0}, yMax{0}, zMin{0}, zMax{0};
         auto plane = Plane{_points.at(0), _points.at(1), _points.at(2)};
         for (auto point : _points)
         {
             if (!plane.conatins(point))
                 throw Exception{"Not all points lie in one plane"};
+            if (point.x() < xMin)
+                xMin = point.x();
+            if (point.x() > xMax)
+                xMax = point.x();
+            if (point.y() < yMin)
+                yMin = point.y();
+            if (point.y() > yMax)
+                yMax = point.y();
+            if (point.z() < zMin)
+                zMin = point.z();
+            if (point.z() > zMax)
+                zMax = point.z();
+            _BBox = BoundingBox{Vector{xMin, yMin, zMin}, Vector{xMax, yMax, zMax}};
         }
         Vector l0{_points[0] - _points[1]}, l1{_points[2] - _points[1]};
         _normal = l0.cross(l1);
@@ -26,52 +40,55 @@ namespace raytracer
 
     std::optional<Intersection> Polygon::intersect(const Ray &ray) const
     {
-        Plane plane{_points.at(2), _points.at(1), _points.at(0)};
-        if (_normal * ray.direction() > plane.normal() * ray.direction())
+        if (_BBox.intersect(ray))
         {
-            plane = Plane{_points.at(0), _points.at(1), _points.at(2)};
-        }
-        if (!plane.intersect(ray))
-            return {};
-        auto hit = plane.intersect(ray).value();
-        double nX{std::abs(plane.normal().x())}, nY{std::abs(plane.normal().y())}, nZ{std::abs(plane.normal().z())};
-        int dimToLoose;
-        if (nX >= nY && nX >= nZ)
-        {
-            dimToLoose = 0;
-        }
-        if (nY >= nX && nY >= nZ)
-        {
-            dimToLoose = 1;
-        }
-        if (nZ >= nX && nZ >= nY)
-        {
-            dimToLoose = 2;
-        }
-        auto projectedHit = hit.position().project(dimToLoose);
-        int intersectionCount = 0;
-        Ray2 testRay{projectedHit, Vector2{1, 0}};
-        std::vector<Line2> lines;
-        lines.reserve(_points.size());
-        Vector2 p0 = _points[0].project(dimToLoose);
-        Vector2 lastPoint = _points.at(_points.size() - 1).project(dimToLoose);
-        lines.push_back(Line2{p0, lastPoint});
-        for (size_t i = 1; i < _points.size(); i++)
-        {
-            Vector2 p1{_points[i - 1].project(dimToLoose)}, p2{_points[i].project(dimToLoose)};
-            lines.push_back(Line2{p1, p2});
-        }
-        for (auto line : lines)
-            if (auto hit = line.intersect(testRay))
+            Plane plane{_points.at(2), _points.at(1), _points.at(0)};
+            if (_normal * ray.direction() > plane.normal() * ray.direction())
             {
-                if (!testRay.direction().parallel(line.p2() - line.p1()))
-                    intersectionCount++;
+                plane = Plane{_points.at(0), _points.at(1), _points.at(2)};
             }
-        if (intersectionCount % 2 == 1)
-        {
-            hit.setNormal(plane.normal());
-            hit.setColor(color());
-            return hit;
+            if (!plane.intersect(ray))
+                return {};
+            auto hit = plane.intersect(ray).value();
+            double nX{std::abs(plane.normal().x())}, nY{std::abs(plane.normal().y())}, nZ{std::abs(plane.normal().z())};
+            int dimToLoose;
+            if (nX >= nY && nX >= nZ)
+            {
+                dimToLoose = 0;
+            }
+            if (nY >= nX && nY >= nZ)
+            {
+                dimToLoose = 1;
+            }
+            if (nZ >= nX && nZ >= nY)
+            {
+                dimToLoose = 2;
+            }
+            auto projectedHit = hit.position().project(dimToLoose);
+            int intersectionCount = 0;
+            Ray2 testRay{projectedHit, Vector2{1, 0}};
+            std::vector<Line2> lines;
+            lines.reserve(_points.size());
+            Vector2 p0 = _points[0].project(dimToLoose);
+            Vector2 lastPoint = _points.at(_points.size() - 1).project(dimToLoose);
+            lines.push_back(Line2{p0, lastPoint});
+            for (size_t i = 1; i < _points.size(); i++)
+            {
+                Vector2 p1{_points[i - 1].project(dimToLoose)}, p2{_points[i].project(dimToLoose)};
+                lines.push_back(Line2{p1, p2});
+            }
+            for (auto line : lines)
+                if (auto hit = line.intersect(testRay))
+                {
+                    if (!testRay.direction().parallel(line.p2() - line.p1()))
+                        intersectionCount++;
+                }
+            if (intersectionCount % 2 == 1)
+            {
+                hit.setNormal(plane.normal());
+                hit.setColor(color());
+                return hit;
+            }
         }
         return {};
     }
