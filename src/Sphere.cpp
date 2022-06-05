b3aeaffe4f20a587f9dff07e0af1976b8bf1d9d4
9Christian6 <chrissi.schulz@googlemail.com>
2022-06-05 17:26:58 +0200

    Add BBox check

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -38,28 +38,31 @@ namespace raytracer
 
     std::optional<Intersection> Sphere::intersect(const Ray &ray) const
     {
-        double B, C;
-        B = ray.direction().x() * (ray.origin().x() - _origin.x());
-        B += ray.direction().y() * (ray.origin().y() - _origin.y());
-        B += ray.direction().z() * (ray.origin().z() - _origin.z());
-        B *= 2;
-        C = std::pow((ray.origin().x() - _origin.x()), 2);
-        C += std::pow((ray.origin().y() - _origin.y()), 2);
-        C += std::pow((ray.origin().z() - _origin.z()), 2);
-        C -= std::pow(_radius, 2);
-        double t = std::pow(B, 2) - (4 * C);
-        if (t > Ray::RAY_T_MIN && t < Ray::RAY_T_MAX)
+        if (_BBox.intersect(ray))
         {
-            t = sqrt(t);
-            t = -B - t;
-            if (t <= 0)
-                return {};
-            t /= 2;
-            Vector hit = ray.getPoint(t);
-            Vector normal = (hit - _origin).normalize();
-            double angle = normal.angle(ray.direction());
-            Material mat = static_cast<Material>(*this);
-            return Intersection{ray, t, normal, color(), angle, mat};
+            double B, C;
+            B = ray.direction().x() * (ray.origin().x() - _origin.x());
+            B += ray.direction().y() * (ray.origin().y() - _origin.y());
+            B += ray.direction().z() * (ray.origin().z() - _origin.z());
+            B *= 2;
+            C = std::pow((ray.origin().x() - _origin.x()), 2);
+            C += std::pow((ray.origin().y() - _origin.y()), 2);
+            C += std::pow((ray.origin().z() - _origin.z()), 2);
+            C -= std::pow(_radius, 2);
+            double t = std::pow(B, 2) - (4 * C);
+            if (t > Ray::RAY_T_MIN && t < Ray::RAY_T_MAX)
+            {
+                t = sqrt(t);
+                t = -B - t;
+                if (t <= 0)
+                    return {};
+                t /= 2;
+                Vector hit = ray.getPoint(t);
+                Vector normal = (hit - _origin).normalize();
+                double angle = normal.angle(ray.direction());
+                Material mat = static_cast<Material>(*this);
+                return Intersection{ray, t, normal, color(), angle, mat};
+            }
         }
         return {};
     }
