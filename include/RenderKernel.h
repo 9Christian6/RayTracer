66ae2353d72134cfec4ac367e0731a3e571dcd4a
9Christian6 <chrissi.schulz@googlemail.com>
2022-06-06 15:04:10 +0200

    Implement pixelrendering and add global illumination function

--- include/RenderKernel.h
+++ include/RenderKernel.h
@@ -1,8 +1,11 @@
 #include "Image.hpp"
-#include "Color.hpp"
+#include "Shapeset.hpp"
 #include "Scene.hpp"
-
+namespace raytracer
+{
 #ifndef RENDERKERNEL_H
 #define RENDERKERNEL_H
-void render(const raytracer::Scene *scene);
+    Color renderPixel(int width, int height, int xResolution, int yResolution, const Scene &scene, int bounces);
+    Color globalLight(Intersection &hit, const Scene &scene, int samples);
 #endif // RENDERKERNEL_H
+}
--- src/RenderKernel.cu
+++ src/RenderKernel.cu
@@ -1,10 +1,43 @@
 #include "RenderKernel.h"
-
-__global__ void renderPar()
+#include "Color.hpp"
+#include "Scene.hpp"
+#include "Camera.hpp"
+#include "Ray.hpp"
+#include "Ray2.hpp"
+#include "Light.hpp"
+#include <utility>
+#include "Material.hpp"
+namespace raytracer
 {
-}
+    __global__ void renderPar()
+    {
+    }
 
-void render(const raytracer::Scene *scene)
-{
-    renderPar<<<1, 1>>>();
+    Color renderPixel(int width, int height, int x, int y, const Scene &scene, int bounces)
+    {
+        Color pixel{0, 0, 0};
+        auto ray = scene.camera().makeRay(width, height, Vector2{x, y});
+        if (auto hit = scene.shapes().intersect(ray))
+        {
+            pixel = *hit->color(scene.visibleLights(hit->position()));
+            ray = hit->reflectionRay();
+        }
+        for (size_t bounce = 1; bounce < bounces; bounce++)
+        {
+            if (auto hit = scene.shapes().intersect(ray))
+            {
+                pixel += *hit->color(scene.visibleLights(hit->position())) * hit.value().material()->specularity() * (1 / (double)bounce);
+                ray = hit->reflectionRay();
+            }
+            else
+                break;
+        }
+        return pixel;
+    }
+
+    Color globalLight(Intersection &hit, const Scene &scene, int samples)
+    {
+        auto position{hit.position()};
+        return Color{0, 0, 0};
+    }
 }
