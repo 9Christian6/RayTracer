8cbbf2117180a325bdb5f787860e792c533cee3a
9Christian6 <chrissi.schulz@googlemail.com>
2022-08-28 14:59:52 +0200

    delete experimental files

--- include/CUDA.h
+++
@@ -1,7 +0,0 @@
-#ifndef CUDA_H
-#define CUDA_H
-#include <cuda.h>
-#include <cuda_runtime.h>
-// __global__ void vector_add(double *result, const double *lhs, const double *rhs);
-void test(double *asdf);
-#endif
