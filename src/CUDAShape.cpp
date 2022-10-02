c91c4b9452e5dd1c8aff03908c6523ccc8f8fd0b
9Christian6 <chrissi.schulz@googlemail.com>
2022-10-02 12:23:59 +0200

    Change image to rgb, fix Werror stuff

--- src/CUDAShape.cpp
+++ src/CUDAShape.cpp
@@ -13,7 +13,7 @@ namespace raytracer
     void printImage(const Image &img)
     {
         std::string image;
-        image.append("P2\n");
+        image.append("P3\n");
         auto widthStr = std::to_string(img._width);
         auto heightStr = std::to_string(img._height);
         image.append(widthStr);
@@ -26,11 +26,11 @@ namespace raytracer
             for (int x = 0; x < img._width; x++)
             {
                 auto pixelColor = img._pixels[img._height - y - 1][x];
-                int r, g, b;
-                r = pixelColor._r;
-                g = pixelColor._g;
-                b = pixelColor._b;
-                image.append(std::to_string(r));
+                image.append(std::to_string((int)pixelColor._r));
+                image.append(" ");
+                image.append(std::to_string((int)pixelColor._g));
+                image.append(" ");
+                image.append(std::to_string((int)pixelColor._b));
                 if (x < img._width - 1)
                 {
                     image.append(" ");
@@ -112,18 +112,33 @@ namespace raytracer
         return false;
     }
 
-    std::optional<Vector2> intersectLine(const Line2 &line, const Ray2 &ray)
+    std::vector<Line2> getLines(const Polygon3 &poly, int dimToLoose)
     {
-        auto hit = std::optional<Vector2>();
+        std::vector<Line2> lines;
+        for (size_t i = 0; i < poly._points.size() - 1; i++)
+        {
+            lines.emplace_back(poly._points[i], poly._points[i + 1], dimToLoose);
+        }
+        lines.emplace_back(poly._points.front(), poly._points.back(), dimToLoose);
+        return lines;
+    }
+
+    Intersection intersectLine(const Line2 &line, const Ray2 &ray)
+    {
+        Intersection hit;
         Ray2 rayLine{line._a, line._b - line._a};
         Vector2 lineVec{line._b, line._a};
-        double lineLength = length(line._b - line._a);
+        // double lineLength = length(line._b - line._a);
         if (parallel(lineVec, rayLine._direction))
         {
             std::cout << "parallel\n";
             if (auto t = getT(rayLine, ray._origin))
                 if (*t >= 0)
-                    return scale(rayLine, *t);
+                {
+                    auto hit2 = scale(rayLine, *t);
+                    hit._position = Vector3{hit2._x, hit2._y, 0};
+                    hit._hit = true;
+                }
         }
         if (equals(rayLine._direction._x, 0))
         {
@@ -131,7 +146,9 @@ namespace raytracer
             auto hitPoint = scale(ray, t);
             if (contains(line, hitPoint) && t >= 0)
             {
-                return scale(rayLine, t);
+                auto hit2 = scale(rayLine, t);
+                hit._position = Vector3{hit2._x, hit2._y, 0};
+                hit._hit = true;
             }
         }
         if (!equals(rayLine._direction._x, 0))
@@ -143,9 +160,13 @@ namespace raytracer
             t /= ray._direction._y * rayLine._direction._x - ray._direction._x * rayLine._direction._y;
             auto hitPoint = scale(ray, t);
             if (contains(line, hitPoint) && t >= 0)
-                return scale(ray, t);
+            {
+                // auto hit2 = scale(ray, t);
+                hit._hit = true;
+                hit.lambert = 1;
+            }
         }
-        return {};
+        return hit;
     }
 
     Intersection intersectShape(const TaggedShape &shape, const Ray3 &ray)
@@ -155,7 +176,7 @@ namespace raytracer
         Plane3 plane;
         Sphere3 sphere;
         double denom{0}, t{0}, B, C, nX, nY, nZ;
-        bool contains{false};
+        // bool contains{false};
         Vector3 sum, prod, normal;
         Vector2 p0, lastPoint;
         Ray2 testRay;
@@ -199,7 +220,7 @@ namespace raytracer
         case PLANE:
             plane = shape._plane;
             denom = dotPorduct(plane._normal, ray._direction);
-            contains = orthogonal(plane._normal, plane._origin - ray._origin);
+            // contains = orthogonal(plane._normal, plane._origin - ray._origin);
             if (equals(denom, 0) && !(plane_contains(plane, ray._origin)))
             {
                 intersection._hit = false;
@@ -233,9 +254,9 @@ namespace raytracer
             intersection = intersectShape(TaggedShape{PLANE, Plane3{shape._polygon}, Color3{}}, ray);
             if (!intersection._hit)
                 return intersection;
-            plane = Plane3{shape._polygon};
-            if (intersection.t > 0)
+            if (intersection.t > RAY_T_MIN)
             {
+                plane = Plane3{shape._polygon};
                 nX = std::abs(plane._normal._x);
                 nY = std::abs(plane._normal._y);
                 nZ = std::abs(plane._normal._z);
@@ -266,11 +287,14 @@ namespace raytracer
                     lines.push_back(Line2{p1, p2});
                 }
                 for (auto line : lines)
-                    if (auto hit = intersectLine(line, testRay))
+                {
+                    auto hit = intersectLine(line, testRay);
+                    if (hit._hit)
                     {
                         if (!parallel(testRay._direction, (line._b - line._a)))
                             intersectionCount++;
                     }
+                }
                 if (intersectionCount % 2 == 1)
                 {
                     intersection._normal = plane._normal;
@@ -286,6 +310,7 @@ namespace raytracer
                     intersection._shape = POLYGON;
                 }
             }
+            break;
         default:
             intersection._hit = false;
             break;
@@ -297,7 +322,6 @@ namespace raytracer
     {
         double xR = ((x / (double)width) * 2) - 1;
         double yR = ((y / (double)height) * 2) - 1;
-        auto vec = cam._forward + xR * cam._w * cam._right;
         auto direction = cam._forward + xR * cam._w * cam._right;
         direction = direction + yR * cam._h * cam._up;
         direction = normalize(direction);
@@ -335,6 +359,15 @@ namespace raytracer
         out << "Ray:\n"
             << "origin: " << ray._origin << "\n"
             << "direction: " << ray._direction << "\n";
+        return out;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Vector3 &vec)
+    {
+        out << "Vec:\n"
+            << "x: " << vec._x << "\n"
+            << "y: " << vec._y << "\n"
+            << "z: " << vec._z << "\n";
         return out;
     }
 
