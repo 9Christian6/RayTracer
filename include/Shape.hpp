46df357b77cb7bda39959aaeab46358e8ed7ba42
Christian Schulz <chrissi.schulz@googlemail.com>
2024-03-10 17:39:43 +0100

    format using clang

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -1,21 +1,18 @@
 #pragma once
+#include "BoundingBox.hpp"
 #include <optional>
 
-#include "BoundingBox.hpp"
+namespace raytracer {
+class Intersection;
+class Ray;
+class Shape {
+protected:
+  BoundingBox m_bbox;
 
-namespace raytracer
-{
-    class Intersection;
-    class Ray;
-    class Shape
-    {
-    protected:
-        BoundingBox m_bbox;
-
-    private:
-    public:
-        inline BoundingBox boundingBox() const { return m_bbox; };
-        virtual ~Shape() = default;
-        virtual std::optional<Intersection> intersect(const Ray &ray) const = 0;
-    };
+private:
+public:
+  inline BoundingBox boundingBox() const { return m_bbox; };
+  virtual ~Shape() = default;
+  virtual std::optional<Intersection> intersect(const Ray &ray) const = 0;
+};
 } // namespace raytracer
